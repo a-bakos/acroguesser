e63acabe435ff5f768b97dcde3ae1e150c430a16
a-bakos <bakosattila@fastem.com>
2022-06-07 22:47:31 +0100

    Basic data structures

--- src/gameplay.rs
+++ src/gameplay.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 pub struct Gameplay {
     player_name: String,
     history: Vec<usize>,
@@ -5,13 +6,29 @@ pub struct Gameplay {
     tries: u8,
 }
 
+#[derive(Debug)]
 pub enum Points {
-    Max = 10_u8,
-    Med = 5_u8,
-    Min = 1_u8,
+    Max = 10,
+    Med = 5,
+    Min = 1,
 }
 
 // add_points
 // store_in_history
 // increase_tries
 // store_player_name
+
+impl Gameplay {
+    pub fn new(player_name: String) -> Self {
+        Self {
+            player_name,
+            history: vec![],
+            points: 0,
+            tries: 0,
+        }
+    }
+
+    pub fn add_points(&mut self, points: Points) {
+        self.points += 1; //Points::Max;
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
+use crate::gameplay::Gameplay;
+
+mod gameplay;
 mod journal;
 
 fn main() {
     println!("Hello, world!");
+
+    let game = Gameplay::new(String::from("abakos"));
+
+    println!("{:?}", game);
 }
