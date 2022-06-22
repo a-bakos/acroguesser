275f01f96e72490ee57711a638bee6f1fc55c320
a-bakos <bakosattila@fastem.com>
2022-06-22 20:24:41 +0100

    Extra logic for rand WIP

--- src/journals.rs
+++ src/journals.rs
@@ -25,6 +25,7 @@ impl Journals {
     //pub fn store_journal_list() {}
 
     pub fn get_random_journal(&self) -> &Journal {
+        let mut selection: &Journal;
         let len = self.all.len();
         let mut rng = thread_rng();
         let index: u32 = rng.gen_range(0..=len as u32);
@@ -38,6 +39,12 @@ impl Journals {
         // - must have a title
         // - shouldn't start wit number
 
-        &self.all[index as usize]
+        selection = &self.all[index as usize];
+
+        if selection.title.is_empty() || selection.acronym.is_empty() {
+            println!("select another one");
+        }
+
+        selection
     }
 }
