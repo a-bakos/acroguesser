daf5fb1c314b0e43c4d64fe8566a078c09f5c103
a-bakos <bakosattila@fastem.com>
2022-07-08 21:01:55 +0100

    Add status logger to Player

--- src/player.rs
+++ src/player.rs
@@ -1,5 +1,5 @@
 use crate::misc::process_player_name;
-use crate::traits;
+use crate::traits::{self, Log};
 
 #[derive(Debug)]
 pub struct Player {
@@ -12,6 +12,11 @@ impl Player {
     pub fn new(player_name: String) -> Self {
         let player_name = process_player_name(player_name);
 
-        Self { name: player_name }
+        let player = Self { name: player_name };
+
+        let msg = format!("Player name: {}", &player.name);
+        player.status(msg.as_str());
+
+        player
     }
 }
